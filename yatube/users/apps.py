6dc78af63cad035e4850c5a89d2cc81f96ffb2d3
Artem <102475384+mityasun@users.noreply.github.com>
2022-06-08 10:40:39 +0500

    git commit -m 'Спринт 6. Проект после 2го ревью.'

--- yatube/users/apps.py
+++ yatube/users/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
 
 class UsersConfig(AppConfig):
@@ -7,6 +6,4 @@ class UsersConfig(AppConfig):
     verbose_name = 'Управление пользователями'
 
     def ready(self):
-        from .signals import create_profile, save_profile
-        post_migrate.connect(create_profile, sender=self)
-        post_migrate.connect(save_profile, sender=self)
+        import users.signals

23db02a8da9b9dc4a4f6b30a26831265330a2cf0
Артем Петухов <mityasun@gmail.com>
2022-06-14 22:08:00 +0500

    Спринт 6. Проект после 2го ревью.

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -6,17 +6,17 @@ from .forms import CreationForm, UserUpdateForm
 
 
 def register(request):
-    if request.method == 'POST':
-        form = CreationForm(request.POST or None, files=request.FILES or None)
-        if form.is_valid():
-            form.save()
-            form.cleaned_data.get('username')
-            new_user = authenticate(
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password1'],
-            )
-            login(request, new_user)
-            return redirect('posts:index')
+    """Функция создания пользователя."""
+    form = CreationForm(request.POST or None, files=request.FILES or None)
+    if form.is_valid():
+        form.save()
+        form.cleaned_data.get('username')
+        new_user = authenticate(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+        )
+        login(request, new_user)
+        return redirect('posts:index')
     else:
         form = CreationForm()
     return render(request, 'users/signup.html', {'form': form})
@@ -24,16 +24,15 @@ def register(request):
 
 @login_required
 def change(request):
-    """Функция редактирования профиля."""
-    if request.method == 'POST':
-        form = UserUpdateForm(
-            request.POST or None,
-            files=request.FILES or None,
-            instance=request.user
-        )
-        if form.is_valid():
-            form.save()
-            return redirect('posts:profile', request.user.username)
+    """Функция редактирования пользователя."""
+    form = UserUpdateForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=request.user
+    )
+    if form.is_valid():
+        form.save()
+        return redirect('posts:profile', request.user.username)
     else:
         form = UserUpdateForm(instance=request.user)
     return render(request, 'users/change.html', {'form': form})

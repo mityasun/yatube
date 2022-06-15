a284212ae5774726019aaa01bd7a20036027bfb8
Артем Петухов <mityasun@gmail.com>
2022-06-16 00:04:15 +0500

    Спринт 6. Проект после 2го ревью.

--- yatube/core/templatetags/user_filters.py
+++ yatube/core/templatetags/user_filters.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+from posts.models import Post
 
 register = template.Library()
 
@@ -6,3 +8,20 @@ register = template.Library()
 @register.filter
 def addclass(field, css):
     return field.as_widget(attrs={'class': css})
+
+
+@register.simple_tag
+def total_posts():
+    return Post.objects.count()
+
+
+@register.inclusion_tag('posts/includes/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.objects.order_by('-pub_date')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.objects.annotate(
+        total_comments=Count('comments')).order_by('-pub_date')[:count]

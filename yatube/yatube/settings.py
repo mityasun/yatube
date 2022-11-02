0b8fff2935ec6cf998aaa39cf2ac4aea93ca646c
Артем Петухов <mityasun@gmail.com>
2022-11-03 01:59:11 +0500

    Add REST API and change models, settings, views, templates, tests, README.md

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-value')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
